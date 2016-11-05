93654afc564318a4ac46bcb438a3d8a81e903d8f
Bruno Matic <bruno.matic.zg@gmail.com>
2016-11-05 21:03:24 +0000

    Forgot to wrap inside container-fluid all pages

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,94 +1,96 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <div class="row">
-        {{ Form::open(['url'=>'/user/edit', 'files'=> true ,'method' => 'post']) }}
-        <div class="col-md-3 col-md-offset-0 col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-0">
-            <!--    Profile pic     -->
-            <img class="img-rounded img-responsive" width="100%" height="100%"
-                 src="/storage/uploads/users/{{ $user->img_name }}">
-            <label class="btn btn-default btn-file">
-                Change picture
-                {{ Form::file('img', ['style'=>'display:none']) }}
-            </label>
-            @if ($errors->has('img'))
-                <span class="help-block">
+    <div class="container-fluid">
+        <div class="row">
+            {{ Form::open(['url'=>'/user/edit', 'files'=> true ,'method' => 'post']) }}
+            <div class="col-md-3 col-md-offset-0 col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-0">
+                <!--    Profile pic     -->
+                <img class="img-rounded img-responsive" width="100%" height="100%"
+                     src="/storage/uploads/users/{{ $user->img_name }}">
+                <label class="btn btn-default btn-file">
+                    Change picture
+                    {{ Form::file('img', ['style'=>'display:none']) }}
+                </label>
+                @if ($errors->has('img'))
+                    <span class="help-block">
                     <strong>{{ $errors->first('img') }}</strong>
                 </span>
-            @endif
-        </div>
-        <div class="col-md-9 col-xs-12 col-sm-8">
-            <ul class="list-group">
-                <!--   User name    -->
-                <li class="list-group-item">
-                    <h2>{{ $user->name }}</h2>
-                </li>
+                @endif
+            </div>
+            <div class="col-md-9 col-xs-12 col-sm-8">
+                <ul class="list-group">
+                    <!--   User name    -->
+                    <li class="list-group-item">
+                        <h2>{{ $user->name }}</h2>
+                    </li>
 
-                <!--    Email       -->
-                <li class="list-group-item">
-                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <h4>{{ Form::label('email', 'Email:', ['class'=>'control-label ']) }}</h4>
-                        {{ Form::email('email', $user->email, ['class' => 'form-control']) }}
+                    <!--    Email       -->
+                    <li class="list-group-item">
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <h4>{{ Form::label('email', 'Email:', ['class'=>'control-label ']) }}</h4>
+                            {{ Form::email('email', $user->email, ['class' => 'form-control']) }}
 
-                        @if ($errors->has('email'))
-                            <span class="help-block">
+                            @if ($errors->has('email'))
+                                <span class="help-block">
                                 <strong>{{ $errors->first('email') }}</strong>
                             </span>
-                        @endif
-                    </div>
-                </li>
+                            @endif
+                        </div>
+                    </li>
 
-                <!--  Description   -->
-                <li class="list-group-item">
-                    <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
-                        <h4>{{ Form::label('description', 'About me:', ['class'=>'control-label']) }}</h4>
-                        {{ Form::textarea('description', $user->description, ['class' => 'form-control']) }}
+                    <!--  Description   -->
+                    <li class="list-group-item">
+                        <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
+                            <h4>{{ Form::label('description', 'About me:', ['class'=>'control-label']) }}</h4>
+                            {{ Form::textarea('description', $user->description, ['class' => 'form-control']) }}
 
-                        @if ($errors->has('description'))
-                            <span class="help-block">
+                            @if ($errors->has('description'))
+                                <span class="help-block">
                         <strong>{{ $errors->first('description') }}</strong>
                     </span>
-                        @endif
-                    </div>
-                </li>
+                            @endif
+                        </div>
+                    </li>
 
-                <!--  Politics   -->
-                <li class="list-group-item">
+                    <!--  Politics   -->
+                    <li class="list-group-item">
 
-                    <div class="form-group{{ $errors->has('politics') ? ' has-error' : '' }}">
-                        <h4>{{ Form::label('politics', 'Politics:', ['class'=>'control-label']) }}</h4>
-                        {{ Form::textarea('politics', $user->politics, ['class' => 'form-control']) }}
+                        <div class="form-group{{ $errors->has('politics') ? ' has-error' : '' }}">
+                            <h4>{{ Form::label('politics', 'Politics:', ['class'=>'control-label']) }}</h4>
+                            {{ Form::textarea('politics', $user->politics, ['class' => 'form-control']) }}
 
-                        @if ($errors->has('politics'))
-                            <span class="help-block">
+                            @if ($errors->has('politics'))
+                                <span class="help-block">
                         <strong>{{ $errors->first('politics') }}</strong>
                     </span>
-                        @endif
-                    </div>
+                            @endif
+                        </div>
 
-                </li>
+                    </li>
 
-                <!--  Interests   -->
-                <li class="list-group-item">
+                    <!--  Interests   -->
+                    <li class="list-group-item">
 
-                    <div class="form-group{{ $errors->has('interests') ? ' has-error' : '' }}">
-                        <h4>{{ Form::label('interests', 'Interests:', ['class'=>'control-label']) }}</h4>
-                        {{ Form::textarea('interests', $user->politics, ['class' => 'form-control']) }}
+                        <div class="form-group{{ $errors->has('interests') ? ' has-error' : '' }}">
+                            <h4>{{ Form::label('interests', 'Interests:', ['class'=>'control-label']) }}</h4>
+                            {{ Form::textarea('interests', $user->politics, ['class' => 'form-control']) }}
 
-                        @if ($errors->has('interests'))
-                            <span class="help-block">
+                            @if ($errors->has('interests'))
+                                <span class="help-block">
                         <strong>{{ $errors->first('interests') }}</strong>
                     </span>
-                        @endif
-                    </div>
+                            @endif
+                        </div>
 
-                </li>
-            </ul>
-            <!--    Save button     -->
-            {{ Form::submit('Save changes', ['class'=>'btn btn-default pull-right'])  }}
+                    </li>
+                </ul>
+                <!--    Save button     -->
+                {{ Form::submit('Save changes', ['class'=>'btn btn-default pull-right'])  }}
+            </div>
         </div>
+        {{ Form::close() }}
+
     </div>
-    {{ Form::close() }}
 
 @endsection
--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,60 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3 col-md-offset-0 col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-0">
+                <!--    Profile pic     -->
+                <img class="img-rounded img-responsive" src="/storage/uploads/users/{{ $user->img_name}}">
+            </div>
 
-    <div class="row">
-        <div class="col-md-3 col-md-offset-0 col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-0">
-            <!--    Profile pic     -->
-            <img class="img-rounded img-responsive" src="/storage/uploads/users/{{ $user->img_name}}">
-        </div>
+            <div class="col-md-9 col-xs-12 col-sm-8">
+                <ul class="list-group">
 
-        <div class="col-md-9 col-xs-12 col-sm-8">
-            <ul class="list-group">
-
-                <!--    User name   -->
-                <li class="list-group-item">
-                <h2>{{ $user->name }}</h2>
-                </li>
-
-                <!--    Email   -->
-                @if($user->id==Auth::user()->id)
+                    <!--    User name   -->
                     <li class="list-group-item">
-                        <h4> Email:</h4>
-                        <p>{{ $user->email }}</p>
+                        <h2>{{ $user->name }}</h2>
                     </li>
+
+                    <!--    Email   -->
+                    @if($user->id==Auth::user()->id)
+                        <li class="list-group-item">
+                            <h4> Email:</h4>
+                            <p>{{ $user->email }}</p>
+                        </li>
                 @endif
 
-                <!--    Created at  -->
-                <li class="list-group-item">
-                    <h4>Date joined:</h4>
-                    <p>{{ $user->created_at }}</p>
-                </li>
+                     <!--    Created at  -->
+                    <li class="list-group-item">
+                        <h4>Date joined:</h4>
+                        <p>{{ $user->created_at }}</p>
+                    </li>
 
-                <!--    Description -->
-                <li class="list-group-item">
-                    <h4> About me: </h4>
-                    <p>{{ $user->description }}</p>
-                </li>
+                    <!--    Description -->
+                    <li class="list-group-item">
+                        <h4> About me: </h4>
+                        <p>{{ $user->description }}</p>
+                    </li>
 
-                <!--    Politics    -->
-                <li class="list-group-item">
-                    <h4>Political opinion: </h4>
-                    <p>{{ $user->politics}}</p>
-                </li>
+                    <!--    Politics    -->
+                    <li class="list-group-item">
+                        <h4>Political opinion: </h4>
+                        <p>{{ $user->politics}}</p>
+                    </li>
 
-                <!--    Interests   -->
-                <li class="list-group-item">
-                    <h4>Interests:</h4>
-                    <p>{{ $user->interests }}</p>
-                </li>
-            </ul>
+                    <!--    Interests   -->
+                    <li class="list-group-item">
+                        <h4>Interests:</h4>
+                        <p>{{ $user->interests }}</p>
+                    </li>
+                </ul>
 
 
-            <!--    Edit button -->
-            @if($user->id==Auth::user()->id)
-                <a href={{url('/user/edit')}} class="btn btn-default pull-right"> Change profile </a>
-            @endif
+                <!--    Edit button -->
+                @if($user->id==Auth::user()->id)
+                    <a href={{url('/user/edit')}} class="btn btn-default pull-right"> Change profile </a>
+                @endif
 
+            </div>
         </div>
     </div>
 
