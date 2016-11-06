9b7ae6015ce3acfe566480abefbd99a2fb0057ff
vagrant <vagrant@homestead>
2016-11-06 23:16:54 +0000

    Finished Registration. Added submenu item for User Info. Request to external platform done

--- app/User.php
+++ app/User.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+
     use Notifiable;
 
     /**
@@ -15,7 +16,15 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'id_card',
+        'birth_date'
+    ];
+
+    protected $dates = [
+        'birth_date'
     ];
 
     /**
@@ -24,6 +33,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
+
+    public function votingLocation()
+    {
+        return $this->belongsTo(VotingLocation::class);
+    }
 }
