8f2892f59e5d9b0ddccffd8945699bb506aeb3ed
Bruno Matic <bruno.matic.zg@gmail.com>
2016-11-17 17:42:34 +0000

    Forgot to drop foreing key on migration rollback

--- database/migrations/2016_11_16_180941_add_user_roles_field.php
+++ database/migrations/2016_11_16_180941_add_user_roles_field.php
@@ -28,6 +28,8 @@ class AddUserRolesField extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_role_id_foreign');
+
             $table->dropColumn('role_id');
         });
     }
